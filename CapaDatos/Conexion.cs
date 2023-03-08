77ddc9744756afb4ec6f9dc539c06b77083302b8
JulioAmaya98 <julio984590@gmail.com>
2023-03-07 23:14:10 -0600

    Agregando clase Usuarios

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace CapaDatos
 {
     public class Conexion
     {
+        SqlConnection conn = new SqlConnection("Data Source=JULIO\\SQLEXPRESS;Initial Catalog=clinic;Integrated Security=True");
+
+
+        public SqlConnection abrirConexion()
+        {
+            if (conn.State == ConnectionState.Closed)
+                conn.Open();
+            return conn;
+        }
+        public SqlConnection cerrarConexion()
+        {
+
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+            return conn;
+        }
     }
 }
---
+++ CapaDatos/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Usuarios
+    {
+        Conexion objConn = new Conexion();
+        SqlDataReader leer;
+        DataTable tablaDatos = new DataTable();
+        SqlCommand comando = new SqlCommand();
+    }
+}
