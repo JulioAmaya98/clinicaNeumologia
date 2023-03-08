8b1447e2d31e6790fc1f21ec6ef89841f382d4c2
mysthicl <irvingmachado08@gmail.com>
2023-03-07 23:26:14 -0600

    Agregando clase entidad

---
+++ CapaEntidad/EUsuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    //Clase entidad para manejo de usuarios
+    public class EUsuario
+    {
+        public int id_usuario { get; set; }
+        public string nombre { get; set; }
+        public string pass { get; set; }
+        public string clave { get; set; }
+        public int id_roles { get; set; }
+
+    }
+}
